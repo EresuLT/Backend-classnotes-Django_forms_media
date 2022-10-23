e2b80f5a0e8f909f4621d1ef2e486077c76db4ca
eResult <eresultolu@gmail.com>
2022-10-23 21:53:29 +0300

    first commit

---
+++ student/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import StudentForm
+
+
+def index(request):
+    return render(request, 'student/index.html')
+
+
+def student_page(request):
+    form = StudentForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'student/student.html', context)
